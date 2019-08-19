d1e9c6d64276e32ffcb654b569454d4aca64a9a2
valeriacastelobranco <valeriacastelobranco@gmail.com>
2019-08-19 22:09:00 +0100

    testing the collection return

--- src/main/java/com/nelioalves/backend/resources/CategoryResource.java
+++ src/main/java/com/nelioalves/backend/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.nelioalves.backend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.backend.domain.Category;
 
 @RestController
 @RequestMapping(value="categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Working";
+	public List<Category> list() {
+		
+		Category catComputing = new Category(1, "Computing");
+		Category catOffice = new Category(1, "Office");
+		
+		List<Category> list = new ArrayList<Category>();
+		list.add(catComputing);
+		list.add(catOffice);
+		
+		return list;
 	}
 
 }
