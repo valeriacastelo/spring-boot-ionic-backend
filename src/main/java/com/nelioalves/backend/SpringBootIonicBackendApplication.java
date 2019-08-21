570beaee9395db99725c69e1d1694dfd258c9d9d
valeriacastelobranco <valeriacastelobranco@gmail.com>
2019-08-21 15:43:26 +0100

    adding product and many to many associations

--- src/main/java/com/nelioalves/backend/SpringBootIonicBackendApplication.java
+++ src/main/java/com/nelioalves/backend/SpringBootIonicBackendApplication.java
@@ -1,8 +1,6 @@
 package com.nelioalves.backend;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.backend.domain.Category;
+import com.nelioalves.backend.domain.Product;
 import com.nelioalves.backend.repositories.CategoryRepository;
+import com.nelioalves.backend.repositories.ProductRepository;
 
 @SpringBootApplication
 public class SpringBootIonicBackendApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepo;
+	@Autowired
+	private ProductRepository productRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIonicBackendApplication.class, args);
@@ -24,10 +26,22 @@ public class SpringBootIonicBackendApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Category catComputing = new Category(null, "Computing");
-		Category catOffice = new Category(null, "Office");
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
 		
-		categoryRepo.saveAll(Arrays.asList(catComputing, catOffice));
+		Product p1 = new Product(null, "Computer", 2000.00);
+		Product p2 = new Product(null, "Printer", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
+		categoryRepo.saveAll(Arrays.asList(cat1, cat2));
+		productRepo.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
--- src/main/java/com/nelioalves/backend/domain/Product.java
+++ src/main/java/com/nelioalves/backend/domain/Product.java
@@ -8,30 +8,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Category implements Serializable {
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private Double price;
 	
-	@ManyToMany(mappedBy = "categories")
-	private List<Product> products = new ArrayList<Product>();
+	@ManyToMany
+	@JoinTable(name = "PRODUCT_CATEGORY",
+		joinColumns = @JoinColumn(name =  "product_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id")
+	)
+	private List<Category> categories = new ArrayList<Category>();
 	
-	public Category() {
+	public Product() {
 		
 	}
 	
-	public Category(Integer id, String name) {
+	public Product(Integer id, String name, Double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
@@ -44,12 +52,19 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public List<Product> getProducts() {
-		return products;
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 
 	@Override
@@ -68,7 +83,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -76,5 +91,5 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
