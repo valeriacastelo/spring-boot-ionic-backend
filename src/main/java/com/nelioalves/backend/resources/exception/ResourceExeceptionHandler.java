f987a505737dc730b374766ee478024dd1a554ee
valeriacastelobranco <valeriacastelobranco@gmail.com>
2019-08-24 12:36:24 +0100

    fixing the endpoint, it was giving 500 and the object did not exist

--- src/main/java/com/nelioalves/backend/resources/exception/ResourceExeceptionHandler.java
+++ src/main/java/com/nelioalves/backend/resources/exception/ResourceExeceptionHandler.java
@@ -5,12 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.nelioalves.backend.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExeceptionHandler {
 	
+	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request) {
 		
 		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), exception.getMessage(), System.currentTimeMillis());
